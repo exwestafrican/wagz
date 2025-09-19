6758518116153fbee65bc71346469b16135369f3
tumise <oladele.alade@gmail.com>
2025-09-19 22:14:33 +0100

    setup

---
+++ src/main/java/io/wagz/WagzApplication.java
@@ -0,0 +1,13 @@
+package io.wagz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WagzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WagzApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/wagz/WagzApplicationTests.java
@@ -0,0 +1,13 @@
+package io.wagz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WagzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
