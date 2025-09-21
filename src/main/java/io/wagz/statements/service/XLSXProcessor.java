3a2beb65565cc922559eef6d74dd0efbde145c79
tumise <oladele.alade@gmail.com>
2025-09-21 20:05:37 +0100

    annotate `XLSXProcessor` with service

--- src/main/java/io/wagz/statements/service/XLSXProcessor.java
+++ src/main/java/io/wagz/statements/service/XLSXProcessor.java
@@ -13,7 +13,9 @@ import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.springframework.stereotype.Service;
 
+@Service
 public class XLSXProcessor {
 
   public BankStatement process(File file) {
