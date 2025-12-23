09f86db5bfd996bb5d12e6fe0002e2d37adeda90
tumise <oladele.alade@gmail.com>
2025-12-23 13:57:52 +0000

    add prisma to app modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
   imports: [
@@ -10,6 +11,7 @@ import { AuthModule } from './auth/auth.module';
       isGlobal: true, // Makes ConfigModule available globally
     }),
     AuthModule,
+    PrismaModule,
   ],
   controllers: [AppController],
   providers: [AppService],
