b58cf49c90b392e35b2275fe9068265d04d65fc5
tumise <oladele.alade@gmail.com>
2025-12-23 17:04:18 +0000

    move test helpers

--- src/test-helpers.ts
+++
@@ -1,9 +0,0 @@
-import { TestingModule } from '@nestjs/testing';
-import { setupApp } from './app.setup';
-
-export async function createTestApp(module: TestingModule) {
-  const app = module.createNestApplication();
-  setupApp(app);
-  await app.init();
-  return app;
-}
