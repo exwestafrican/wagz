6942070a91f1d5fbae081c6dc7a2b4421ffe9e5e
tumise <oladele.alade@gmail.com>
2025-12-26 10:03:11 +0000

    refactor test

--- src/common/validators/is-not-disposable-email.decorator.spec.ts
+++ src/common/validators/is-not-disposable-email.decorator.spec.ts
@@ -11,25 +11,29 @@ class TestDto {
   email: string;
 }
 
+const disposableEmails = [
+  '10minutemail.com',
+  'guerrillamail.com',
+  'mailinator.com',
+  'tempmail.com',
+  'throwaway.email',
+  'temp-mail.org',
+  'getnada.com',
+  'mohmal.com',
+  'yopmail.com',
+  'sharklasers.com',
+];
 describe('IsNotDisposableEmail', () => {
-  test.each([
-    '10minutemail.com',
-    'guerrillamail.com',
-    'mailinator.com',
-    'tempmail.com',
-    'throwaway.email',
-    'temp-mail.org',
-    'getnada.com',
-    'mohmal.com',
-    'yopmail.com',
-    'sharklasers.com',
-  ])('it should fail for domain %s', async (domain: string) => {
-    const dto = new TestDto();
-    dto.email = `user@${domain}`;
-    const errors = await validate(dto);
+  test.each(disposableEmails)(
+    'it should fail for domain %s',
+    async (domain: string) => {
+      const dto = new TestDto();
+      dto.email = `user@${domain}`;
+      const errors = await validate(dto);
 
-    expect(errors).toHaveLength(1);
-  });
+      expect(errors).toHaveLength(1);
+    },
+  );
 
   test.each(['usenvoye.com', 'google.com', 'outlook.com'])(
     'it allows domain %s',
@@ -44,21 +48,13 @@ describe('IsNotDisposableEmail', () => {
   describe('IsNotDisposableEmailConstraint', () => {
     const constraint = new IsNotDisposableEmailConstraint();
 
-    test.each([
-      '10minutemail.com',
-      'guerrillamail.com',
-      'mailinator.com',
-      'tempmail.com',
-      'throwaway.email',
-      'temp-mail.org',
-      'getnada.com',
-      'mohmal.com',
-      'yopmail.com',
-      'sharklasers.com',
-    ])('it should fail for domain %s', (domain: string) => {
-      const email = `user@${domain}`;
-      expect(constraint.validate(email)).toBe(false);
-    });
+    test.each(disposableEmails)(
+      'it should fail for domain %s',
+      (domain: string) => {
+        const email = `user@${domain}`;
+        expect(constraint.validate(email)).toBe(false);
+      },
+    );
 
     test.each(['usenvoye.com', 'google.com', 'outlook.com'])(
       'it allows domain %s',
