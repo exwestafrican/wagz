3d58b160034812c54771c3f3074b3a5c3925f7c3
tumise <oladele.alade@gmail.com>
2025-12-26 10:00:01 +0000

    update import

--- src/roadmap/roadmap.controller.spec.ts
+++ src/roadmap/roadmap.controller.spec.ts
@@ -2,7 +2,6 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { RoadmapController } from '@/roadmap/roadmap.controller';
 import { RoadmapEndpoints } from '@/roadmap/consts';
 import { INestApplication } from '@nestjs/common';
-import { Server } from 'http';
 import { createTestApp } from '@/test-helpers/test-app';
 import request from 'supertest';
 import { FeaturesService } from '@/roadmap/service/feature.service';
@@ -10,7 +9,7 @@ import { PrismaService } from '@/prisma/prisma.service';
 import { ConfigService } from '@nestjs/config';
 import { FeatureStage } from '@/generated/prisma/enums';
 import { FeatureDto } from '@/roadmap/dto/feature.dto';
-import featureFactory from '@factories/roadmap/features.factory';
+import featureFactory from '@/factories/roadmap/features.factory';
 import getHttpServer from '@/test-helpers/get-http-server';
 
 describe('RoadmapController', () => {
