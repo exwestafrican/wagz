fbfdf9b900cd0aba2573b46ecc47359e4c56a041
tumise <63903646+exwestafrican@users.noreply.github.com>
2025-12-30 19:59:37 +0000

    Update seed file (#37)
    update seed file

--- supabase/seed.sql
+++ supabase/seed.sql
@@ -1,6 +1,6 @@
 INSERT INTO "Feature" (id, name, votes, icon, stage, "createdAt", "updatedAt")
 VALUES
-  ('b4dfec35-1c7f-4f6d-8f87-8bcd766df26c', 'whatsApp inbox integration',           0, 'message-square', 'PLANNED',       CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-  (gen_random_uuid(), 'receive Gmail emails in your inbox',   2, 'mail',           'IN_PROGRESS',   CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-  (gen_random_uuid(), 'send and receive images in chat',      1, 'image',          'IN_PROGRESS',   CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-  (gen_random_uuid(), 'receive Instagram messages in your inbox', 3, 'message-square', 'IN_PROGRESS', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+  ('b4dfec35-1c7f-4f6d-8f87-8bcd766df26c', 'whatsApp inbox integration',           0, 'message-circle', 'PLANNED',       CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+  (gen_random_uuid(), 'receive Gmail emails in your inbox',   1, 'mail',           'IN_PROGRESS',   CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+  (gen_random_uuid(), 'send and receive images in chat',      3, 'image',          'IN_PROGRESS',   CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+  (gen_random_uuid(), 'receive Instagram messages in your inbox', 2, 'message-square', 'IN_PROGRESS', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
