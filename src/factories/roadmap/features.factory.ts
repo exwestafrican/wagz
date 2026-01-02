c2792a69b64cd83831b396733784a669e6e9ac98
tumise <oladele.alade@gmail.com>
2026-01-02 18:03:57 +0000

    fix factory

--- src/factories/roadmap/features.factory.ts
+++ src/factories/roadmap/features.factory.ts
@@ -12,7 +12,7 @@ const featureFactory = Factory.define<Feature, FeatureTransientParams>(
     return {
       id: faker.string.uuid(),
       name: transientParams.main ? MAIN_FEATURE : faker.lorem.words(3),
-      votes: faker.number.int({ min: 0, max: 100 }),
+      voteCount: faker.number.int({ min: 0, max: 100 }),
       icon: faker.string.alpha(15),
       stage: faker.helpers.arrayElement(Object.values(FeatureStage)),
       createdAt: faker.date.past(),
