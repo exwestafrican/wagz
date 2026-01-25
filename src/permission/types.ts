395edb8446828f5952b32f1fc0b28598cafe4bbe
tumise <oladele.alade@gmail.com>
2026-01-25 19:00:10 +0000

    fix grammar

--- src/permission/types.ts
+++ src/permission/types.ts
@@ -4,22 +4,22 @@ import { Role } from '@/permission/domain/role';
 export const PERMISSIONS = {
   READ_SUPPORT_CONVERSATIONS: Permission.of(
     'Read Support Messages',
-    'Allows user view support messages in the inbox',
-    'read_support_messages',
+    'Allows user to view support messages in the inbox',
+    'read_support_conversations',
   ),
   REPLY_SUPPORT_CONVERSATIONS: Permission.of(
     'Reply to Support Messages',
-    'Allows user send replies to support messages',
-    'reply_support_messages',
+    'Allows user to send replies to support messages',
+    'reply_support_conversations',
   ),
   MANAGE_TEAMMATES: Permission.of(
     'Add or Remove Team Members',
-    'Allows user add or Remove Team Members',
+    'Allows user to add or remove team members',
     'manage_teammates',
   ),
   MANAGE_CHANNELS: Permission.of(
     'Connect Messaging Platforms',
-    'Allows user connect supported messaging platforms, like whatsapp etc',
+    'Allows user to connect supported messaging platforms, like whatsapp etc',
     'manage_channels',
   ),
 } as const;
