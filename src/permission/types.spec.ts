66e6aa1b758328c6900ed8c3a737806c3c97a987
tumise <oladele.alade@gmail.com>
2026-01-25 19:06:17 +0000

    update test

--- src/permission/types.spec.ts
+++ src/permission/types.spec.ts
@@ -7,6 +7,27 @@ describe('types', () => {
       const uniqueCodes = new Set(codes);
       expect(codes.length).toBe(uniqueCodes.size);
     });
+
+    it('should have non-empty codes', () => {
+      Object.values(PERMISSIONS).forEach((permission) => {
+        expect(permission.code).toBeTruthy();
+        expect(permission.code.length).toBeGreaterThan(0);
+      });
+    });
+
+    it('should have non-empty names', () => {
+      Object.values(PERMISSIONS).forEach((permission) => {
+        expect(permission.name).toBeTruthy();
+        expect(permission.name.length).toBeGreaterThan(0);
+      });
+    });
+
+    it('should have non-empty description', () => {
+      Object.values(PERMISSIONS).forEach((permission) => {
+        expect(permission.description).toBeTruthy();
+        expect(permission.description.length).toBeGreaterThan(0);
+      });
+    });
   });
 
   describe('roles', () => {
@@ -14,6 +35,13 @@ describe('types', () => {
       const codes = Object.values(ROLES).map((r) => r.code);
       const uniqueCodes = new Set(codes);
       expect(codes.length).toBe(uniqueCodes.size);
+    });
+
+    it('should have non-empty code', () => {
+      Object.values(ROLES).forEach((roles) => {
+        expect(roles.code).toBeTruthy();
+        expect(roles.code.length).toBeGreaterThan(0);
+      });
     });
   });
 });
