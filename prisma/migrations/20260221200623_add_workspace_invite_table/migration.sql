5a3cf59fb94a2bc0bc2dc1c33585c148f9bcd279
tumise <oladele.alade@gmail.com>
2026-02-21 20:09:46 +0000

    add unique index to workspace code and role to workspaceInvite

--- prisma/migrations/20260221200623_add_workspace_invite_table/migration.sql
+++ prisma/migrations/20260221200623_add_workspace_invite_table/migration.sql
@@ -5,10 +5,11 @@ CREATE TYPE "InviteStatus" AS ENUM ('PENDING', 'SENT', 'FAILED', 'ACCEPTED');
 CREATE TABLE "workspace_invite" (
     "id" SERIAL NOT NULL,
     "recipientEmail" VARCHAR(255) NOT NULL,
-    "workspaceCode" INTEGER NOT NULL,
+    "workspaceCode" TEXT NOT NULL,
     "inviteCode" VARCHAR(6) NOT NULL,
     "status" "InviteStatus" NOT NULL DEFAULT 'PENDING',
     "senderId" INTEGER NOT NULL,
+    "recipientRole" TEXT[] DEFAULT ARRAY[]::TEXT[],
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "validTill" TIMESTAMP(3) NOT NULL,
     "acceptedAt" TIMESTAMP(3),
@@ -20,7 +21,7 @@ CREATE TABLE "workspace_invite" (
 CREATE INDEX "workspace_invite_workspaceCode_idx" ON "workspace_invite"("workspaceCode");
 
 -- AddForeignKey
-ALTER TABLE "workspace_invite" ADD CONSTRAINT "workspace_invite_workspaceCode_fkey" FOREIGN KEY ("workspaceCode") REFERENCES "workspace"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "workspace_invite" ADD CONSTRAINT "workspace_invite_workspaceCode_fkey" FOREIGN KEY ("workspaceCode") REFERENCES "workspace"("code") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "workspace_invite" ADD CONSTRAINT "workspace_invite_senderId_fkey" FOREIGN KEY ("senderId") REFERENCES "teammate"("id") ON DELETE CASCADE ON UPDATE CASCADE;
