fba65e5cb2c288a513e00099fae3a71adc8e010c
tumise <oladele.alade@gmail.com>
2026-02-28 11:41:38 +0000

    setup mock for template

--- src/workspace/workspace-manager.service.spec.ts
+++ src/workspace/workspace-manager.service.spec.ts
@@ -24,6 +24,9 @@ import teammateFactory from '@/factories/teammate.factory';
 import workspaceInviteFactory from '@/factories/workspace-invite.factory';
 import { MessagingModule } from '@/messaging/messaging.module';
 
+jest.mock('@react-email/render', () => ({
+  render: jest.fn().mockResolvedValue('<html>Mock Email</html>'),
+}));
 describe('WorkspaceService', () => {
   let service: WorkspaceManager;
   let app: INestApplication;
