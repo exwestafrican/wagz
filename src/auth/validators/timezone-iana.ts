9ef5557f525b0779632d4bcf211ef27f8eb6c2e1
Akomere Akpoveso <akomere.peter@gmail.com>
2026-03-20 15:40:12 +0000

    Fix lint errors

--- src/auth/validators/timezone-iana.ts
+++ src/auth/validators/timezone-iana.ts
@@ -16,5 +16,3 @@ export class IsValidIANATimezoneConstraint implements ValidatorConstraintInterfa
   }
 }
 
-
-eslint/eslintrc, @eslint/js, @faker-js/faker, @nestjs/cli, @nestjs/schematics, @nestjs/testing, @react-email/preview-server, @testcontainers/postgresql, @types/express, @types/generate-password, @types/jest, @types/node, @types/passport-local, @types/react, @types/react-dom, @types/supertest, dotenv, eslint, eslint-config-prettier, eslint-plugin-prettier, fishery, globals, jest, prettier, prisma, react-email, source-map-support, supabase, supertest, testcontainers, ts-jest, ts-loader, ts-node, tsconfig-paths, typescript, typescript-eslint, @nestjs/common, @nestjs/config, @nestjs/core, @nestjs/mapped-types, @nestjs/passport, @nestjs/platform-express, @nestjs/swagger, @nestjs/typeorm, @prisma/adapter-pg, @prisma/client, @react-email/components, @react-email/render, @supabase/supabase-js, @types/moment-timezone, @types/nodemailer, class-transformer, class-validator, generate-password, jose, libphonenumber-js, moment-timezone, nodemailer, passport, pg, react, react-dom, reflect-metadata, rxjs, swagger-ui-express, typeorm, zeptomail
