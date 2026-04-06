77543f81bf96ce50ccbd59bee5cb1bb8eecd1589
tumise <oladele.alade@gmail.com>
2026-04-06 18:54:38 +0100

    fix module loading

--- src/workspace/workspace.module.ts
+++ src/workspace/workspace.module.ts
@@ -3,11 +3,12 @@ import { WorkspaceManager } from './workspace-manager.service';
 import { WorkspaceController } from './workspace.controller';
 import { WorkspaceLinkService } from '@/workspace/workspace-link.service';
 import { PermissionModule } from '@/permission/permission.module';
+import { WorkspaceInviteService } from '@/workspace/workspace-invite-service';
 
 @Module({
   imports: [PermissionModule, PermissionModule],
-  providers: [WorkspaceManager, WorkspaceLinkService],
+  providers: [WorkspaceManager, WorkspaceLinkService, WorkspaceInviteService],
   controllers: [WorkspaceController],
-  exports: [WorkspaceManager, WorkspaceLinkService],
+  exports: [WorkspaceManager, WorkspaceLinkService, WorkspaceInviteService],
 })
 export class WorkspaceModule {}
