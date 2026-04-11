35c3a25d0fc1748574921fdfbdfe8ef0665ece32
tumise <oladele.alade@gmail.com>
2026-04-11 13:05:28 +0100

    remove comment

--- prisma/migrations/20260411120128_add_unique_workspace_username_on_teammate/migration.sql
+++ prisma/migrations/20260411120128_add_unique_workspace_username_on_teammate/migration.sql
@@ -1,8 +1,2 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[workspaceCode,username]` on the table `teammate` will be added. If there are existing duplicate values, this will fail.
-
-*/
 -- CreateIndex
 CREATE UNIQUE INDEX "teammate_workspaceCode_username_key" ON "teammate"("workspaceCode", "username");
