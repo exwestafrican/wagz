475fb40cb7b610c4fbe552d05d8692b30f672290
tumise <oladele.alade@gmail.com>
2026-04-25 14:56:18 +0100

    fix

--- src/common/common.module.ts
+++ src/common/common.module.ts
@@ -1,14 +1,6 @@
 import { Module } from '@nestjs/common';
 import { LinkService } from '@/common/link-service';
-import DebounceService from '@/common/debounce.service';
-import { DEBOUNCE_SERVICE } from '@/common/debounce.service';
-
-const DebounceServiceProvider = {
-  provide: DEBOUNCE_SERVICE,
-  useFactory: () => {
-    return new DebounceService();
-  },
-};
+import { DebounceServiceProvider } from '@/common/debounce.service';
 
 @Module({
   providers: [LinkService, DebounceServiceProvider],
