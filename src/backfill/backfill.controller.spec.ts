c23f553b83e382825f31fb0a1c926a96a27d8a6e
tumise <oladele.alade@gmail.com>
2026-04-25 08:30:20 +0100

    fix test

--- src/backfill/backfill.controller.spec.ts
+++ src/backfill/backfill.controller.spec.ts
@@ -11,33 +11,74 @@ import { HttpStatus, INestApplication } from '@nestjs/common';
 import request from 'supertest';
 import { URIPaths } from '@/common/const';
 import BackfillResponseDto from '@/backfill/dto/backfill-response.dto';
-import { ENVOYE_WORKSPACE_CODE } from '@/feature-flag/const';
+import { PermissionService } from '@/permission/permission.service';
+import { RoleService } from '@/permission/role/role.service';
+import {
+  setupWorkspaceWithTeammate,
+} from '@/test-helpers/workspace-helpers';
+import teammateFactory from '@/factories/teammate.factory';
+import { ROLES } from '@/permission/types';
+import { PrismaService } from '@/prisma/prisma.service';
+import Factory, { PersistStrategy } from '@/factories/factory';
 
 describe('BackfillController', () => {
   let requestUser: RequestUser;
   let app: INestApplication;
+  let prismaService: PrismaService;
+  let factory: PersistStrategy;
 
   beforeEach(async () => {
     requestUser = RequestUser.of('sam@useEnvoye.co');
     const module: TestingModule = await TestControllerModuleWithAuthUser({
       controllers: [BackfillController],
-      providers: [BackfillRegistryProvider],
+      providers: [
+        BackfillRegistryProvider,
+        PermissionService,
+        RoleService,
+        PrismaService,
+      ],
     }).with(requestUser);
-
     app = await createTestApp(module);
+    prismaService = app.get<PrismaService>(PrismaService);
+    factory = Factory.createStrategy(prismaService);
   });
 
   describe('list', () => {
-    it('should return a list of backfill jobs', async () => {
+    it('should return a list of backfill jobs for super admin', async () => {
+      const { workspace } = await setupWorkspaceWithTeammate(
+        factory,
+        teammateFactory.build({
+          email: requestUser.email,
+          groups: [ROLES.SuperAdmin.code],
+        }),
+      );
       const response = await request(getHttpServer(app))
         .get(URIPaths.LIST_TASKS)
         .set('Accept', 'application/json')
         .set('Authorization', 'Bearer test-token')
-        .query({ workspaceCode: ENVOYE_WORKSPACE_CODE })
+        .query({ workspaceCode: workspace.code })
         .send()
         .expect(HttpStatus.OK);
       const body = response.body as BackfillResponseDto[];
       expect(body.length).toBe(1);
+    });
+
+    it('should return 403 for when user is not super admin', async () => {
+      const { workspace } = await setupWorkspaceWithTeammate(
+        factory,
+        teammateFactory.build({
+          email: requestUser.email,
+          groups: [ROLES.WorkspaceAdmin.code],
+        }),
+      );
+
+      await request(getHttpServer(app))
+        .get(URIPaths.LIST_TASKS)
+        .set('Accept', 'application/json')
+        .set('Authorization', 'Bearer test-token')
+        .query({ workspaceCode: workspace.code })
+        .send()
+        .expect(HttpStatus.FORBIDDEN);
     });
   });
 });
