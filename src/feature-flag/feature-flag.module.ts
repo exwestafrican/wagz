f4bd7fbc92a6632297bfd446d703bbe3f80e4be0
tumise <oladele.alade@gmail.com>
2026-05-01 14:49:39 +0100

    Clean up Deprecated Feature Flag Service

--- src/feature-flag/feature-flag.module.ts
+++ src/feature-flag/feature-flag.module.ts
@@ -1,21 +1,8 @@
 import { Module } from '@nestjs/common';
-import { DeprecatedFeatureFlagService } from './deprecated-feature-flag.service';
-import { FEATURE_FLAG_LOADER } from './const';
-import { LocalFeatureFlagLoader } from './service/local-feature-flag-loader';
 import FeatureFlagManager from '@/feature-flag/manager';
 
-const FeatureFlagLoaderProvider = {
-  provide: FEATURE_FLAG_LOADER,
-  useFactory: () => {
-    return new LocalFeatureFlagLoader();
-  },
-};
 @Module({
-  providers: [
-    DeprecatedFeatureFlagService,
-    FeatureFlagManager,
-    FeatureFlagLoaderProvider,
-  ],
-  exports: [FEATURE_FLAG_LOADER, FeatureFlagManager],
+  providers: [FeatureFlagManager],
+  exports: [FeatureFlagManager],
 })
 export class FeatureFlagModule {}
