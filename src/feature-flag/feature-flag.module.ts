06244238b9cc0652fecb03b58ae07884d9d78101
tumise <oladele.alade@gmail.com>
2026-05-01 13:51:46 +0100

    fix ci

--- src/feature-flag/feature-flag.module.ts
+++ src/feature-flag/feature-flag.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { DeprecatedFeatureFlagService } from './deprecated-feature-flag.service';
 import { FEATURE_FLAG_LOADER } from './const';
 import { LocalFeatureFlagLoader } from './service/local-feature-flag-loader';
-import FeatureFlagManager from './feature-flag.service';
+import FeatureFlagManager from '@/feature-flag/manager';
 
 const FeatureFlagLoaderProvider = {
   provide: FEATURE_FLAG_LOADER,
