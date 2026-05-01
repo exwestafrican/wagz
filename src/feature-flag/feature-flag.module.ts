c186da7aa9e8ad3bc4c9865e089d406255ba68eb
Tumise Alade <oladele.alade@gmail.com>
2026-05-01 15:08:01 +0100

    Clean up Deprecated Feature Flag Service (#177)
    * Clean up Deprecated Feature Flag Service
    
    * fix issue

--- src/feature-flag/feature-flag.module.ts
+++ src/feature-flag/feature-flag.module.ts
@@ -1,21 +1,10 @@
 import { Module } from '@nestjs/common';
-import { DeprecatedFeatureFlagService } from './deprecated-feature-flag.service';
-import { FEATURE_FLAG_LOADER } from './const';
-import { LocalFeatureFlagLoader } from './service/local-feature-flag-loader';
 import FeatureFlagManager from '@/feature-flag/manager';
+import { FeatureFlagController } from '@/feature-flag/feature-flag.controller';
 
-const FeatureFlagLoaderProvider = {
-  provide: FEATURE_FLAG_LOADER,
-  useFactory: () => {
-    return new LocalFeatureFlagLoader();
-  },
-};
 @Module({
-  providers: [
-    DeprecatedFeatureFlagService,
-    FeatureFlagManager,
-    FeatureFlagLoaderProvider,
-  ],
-  exports: [FEATURE_FLAG_LOADER, FeatureFlagManager],
+  providers: [FeatureFlagManager],
+  controllers: [FeatureFlagController],
+  exports: [FeatureFlagManager],
 })
 export class FeatureFlagModule {}
--- src/test-helpers/workspace-helpers.ts
+++ src/test-helpers/workspace-helpers.ts
@@ -1,7 +1,11 @@
-import { PersistStrategy } from '@/factories/factory';
+import Factory, { PersistStrategy } from '@/factories/factory';
 import teammateFactory from '@/factories/teammate.factory';
 import workspaceFactory from '@/factories/workspace.factory';
-import { Teammate } from '@/generated/prisma/client';
+import { FeatureFlagStatus, Teammate } from '@/generated/prisma/client';
+import { INestApplication } from '@nestjs/common';
+import { PrismaService } from '@/prisma/prisma.service';
+import FeatureFlagManager from '@/feature-flag/manager';
+import featureFlagFactory from '@/factories/feature-flag.factory';
 
 export async function setupWorkspaceWithTeammateRole(
   factory: PersistStrategy,
@@ -30,4 +34,29 @@ export async function setupWorkspaceWithTeammate(
   );
   const createdTeammate = await factory.persist('teammate', () => teammate);
   return { workspace: workspace, teammate: createdTeammate };
+}
+
+export async function setupWorkspaceWithFeatures(
+  app: INestApplication,
+  workspaceCode: string,
+  featureKeys: string[],
+) {
+  const prismaService = app.get<PrismaService>(PrismaService);
+  const featureFlagManager: FeatureFlagManager =
+    app.get<FeatureFlagManager>(FeatureFlagManager);
+  const factory = Factory.createStrategy(prismaService);
+
+  await factory.persist('workspace', () =>
+    workspaceFactory.build({ code: workspaceCode }),
+  );
+
+  for (const key of featureKeys) {
+    await factory.persist('featureFlag', () =>
+      featureFlagFactory.build({
+        key,
+        status: FeatureFlagStatus.PARTIAL,
+      }),
+    );
+    await featureFlagManager.turnOnFF(workspaceCode, key);
+  }
 }
