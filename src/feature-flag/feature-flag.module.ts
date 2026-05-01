e2b7f578d76df94342141391285579233c39a41c
tumise <oladele.alade@gmail.com>
2026-05-01 15:04:29 +0100

    fix issue

--- src/feature-flag/feature-flag.module.ts
+++ src/feature-flag/feature-flag.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import FeatureFlagManager from '@/feature-flag/manager';
+import { FeatureFlagController } from '@/feature-flag/feature-flag.controller';
 
 @Module({
   providers: [FeatureFlagManager],
+  controllers: [FeatureFlagController],
   exports: [FeatureFlagManager],
 })
 export class FeatureFlagModule {}
