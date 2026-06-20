a367f3786eb060d66abeefe1167669b9605bb69b
tumise <oladele.alade@gmail.com>
2026-06-20 14:41:29 +0100

    edit migration file

--- prisma/migrations/20260620133536_add_author_to_message/migration.sql
+++ prisma/migrations/20260620133536_add_author_to_message/migration.sql
@@ -1,9 +1,3 @@
-/*
-  Warnings:
-
-  - Added the required column `authorId` to the `message` table without a default value. This is not possible if the table is not empty.
-
-*/
 -- AlterTable
 ALTER TABLE "message" ADD COLUMN     "authorId" INTEGER NOT NULL;
 
