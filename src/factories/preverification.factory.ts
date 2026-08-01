8d5c86254be7e6db4cc24750bab5e4657ab7eef3
tumise <oladele.alade@gmail.com>
2026-08-01 17:44:30 +0100

    fix

--- src/factories/preverification.factory.ts
+++ src/factories/preverification.factory.ts
@@ -7,26 +7,28 @@ import { Factory } from 'fishery';
 import { PrismaService } from '@/prisma/prisma.service';
 import buildUsername from '@/common/build-username';
 
-const preVerificationFactory = Factory.define<PreVerification>(() => {
-  const firstName = faker.person.firstName();
-  const lastName = faker.person.lastName();
-  const username = buildUsername(firstName, lastName);
+const preVerificationFactory = Factory.define<PreVerification>(
+  ({ params }) => {
+    const firstName = params.firstName ?? faker.person.firstName();
+    const lastName = params.lastName ?? faker.person.lastName();
+    const username = params.username ?? buildUsername(firstName, lastName);
 
-  return {
-    id: faker.string.uuid(),
-    email: faker.internet.email(),
-    firstName: firstName,
-    lastName: lastName,
-    companyName: faker.company.name(),
-    username: username,
-    phoneCountryCode: '+234',
-    phoneNumber: '8169098834',
-    status: PreVerificationStatus.PENDING,
-    createdAt: faker.date.past(),
-    updatedAt: faker.date.recent(),
-    timezone: 'Africa/Lagos',
-  };
-});
+    return {
+      id: faker.string.uuid(),
+      email: faker.internet.email(),
+      firstName: firstName,
+      lastName: lastName,
+      companyName: faker.company.name(),
+      username: username,
+      phoneCountryCode: '+234',
+      phoneNumber: '8169098834',
+      status: PreVerificationStatus.PENDING,
+      createdAt: faker.date.past(),
+      updatedAt: faker.date.recent(),
+      timezone: 'Africa/Lagos',
+    };
+  },
+);
 
 export async function persistPreVerification(
   prismaService: PrismaService,
--- src/factories/roadmap/preverification.factory.ts
+++ src/factories/roadmap/preverification.factory.ts
@@ -7,25 +7,27 @@ import { faker } from '@faker-js/faker';
 import { PrismaService } from '@/prisma/prisma.service';
 import buildUsername from '@/common/build-username';
 
-const preVerificationFactory = Factory.define<PreVerification>(() => {
-  const firstName = faker.person.firstName();
-  const lastName = faker.person.lastName();
-  const username = buildUsername(firstName, lastName);
-  return {
-    id: faker.string.uuid(),
-    email: faker.internet.email(),
-    firstName: firstName,
-    lastName: lastName,
-    companyName: faker.company.name(),
-    username: username,
-    phoneCountryCode: '+234',
-    phoneNumber: '8169098834',
-    status: PreVerificationStatus.PENDING,
-    createdAt: faker.date.past(),
-    updatedAt: faker.date.recent(),
-    timezone: 'Africa/Lagos',
-  };
-});
+const preVerificationFactory = Factory.define<PreVerification>(
+  ({ params }) => {
+    const firstName = params.firstName ?? faker.person.firstName();
+    const lastName = params.lastName ?? faker.person.lastName();
+    const username = params.username ?? buildUsername(firstName, lastName);
+    return {
+      id: faker.string.uuid(),
+      email: faker.internet.email(),
+      firstName: firstName,
+      lastName: lastName,
+      companyName: faker.company.name(),
+      username: username,
+      phoneCountryCode: '+234',
+      phoneNumber: '8169098834',
+      status: PreVerificationStatus.PENDING,
+      createdAt: faker.date.past(),
+      updatedAt: faker.date.recent(),
+      timezone: 'Africa/Lagos',
+    };
+  },
+);
 
 export async function persistPreverificationStrategy(
   prismaService: PrismaService,
