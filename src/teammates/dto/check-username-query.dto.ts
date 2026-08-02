d497e7c2fb33ab604f9b2ad4f75920e17c5cbfda
tumise <oladele.alade@gmail.com>
2026-08-02 09:22:03 +0100

    fix lint

--- src/teammates/dto/check-username-query.dto.ts
+++ src/teammates/dto/check-username-query.dto.ts
@@ -1,5 +1,5 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsNotEmpty, IsOptional, IsString } from 'class-validator';
+import { IsOptional, IsString } from 'class-validator';
 import {
   IsValidUsername,
   USERNAME_MAX_LENGTH,
