dbc8859c2293c14406271281fa76e94e896df106
tumise <oladele.alade@gmail.com>
2026-08-15 22:23:45 +0100

    temporary apply to unblock

--- src/tracker/tracker.service.ts
+++ src/tracker/tracker.service.ts
@@ -14,7 +14,7 @@ export class TrackerService {
   async registerDevice(imei: string): Promise<Device> {
     try {
       const device = await this.prismaService.device.create({
-        data: { imei },
+        data: { imei, apiKeyHash: `pending_${imei}` }, // temporary to unblock myself
       });
       this.logger.log(`registered device id=${device.id} imei=${imei}`);
       return device;
