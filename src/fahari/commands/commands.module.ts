bd6914d8b3c641fb829ee9ba4fec177c9c9b6b30
Tumise Alade <oladele.alade@gmail.com>
2026-09-06 08:15:32 +0100

    Add command to setup super admin (#319)
    * Add command to setup super admin
    
    * fix lint

---
+++ src/fahari/commands/commands.module.ts
@@ -0,0 +1,23 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { createClient, SupabaseClient } from '@supabase/supabase-js';
+import { CreateSuperAdminCommand } from '@/fahari/commands/create-super-admin.command';
+import { PrismaModule } from '@/prisma/prisma.module';
+
+const supabaseAuthClient = {
+  provide: SupabaseClient,
+  inject: [ConfigService],
+  useFactory: (configService: ConfigService) => {
+    return createClient(
+      configService.get<string>('SUPABASE_URL', ''),
+      configService.get<string>('SUPABASE_KEY', ''),
+    );
+  },
+};
+
+@Module({
+  imports: [PrismaModule, ConfigModule],
+  providers: [CreateSuperAdminCommand, supabaseAuthClient],
+  exports: [CreateSuperAdminCommand],
+})
+export class CommandsModule {}
